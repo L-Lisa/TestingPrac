84231e199afe568e64e94948156d67d6990e8011
L-Lisa <lisaojeland@gmail.com>
2020-11-19 20:23:03 +0100

    start

--- src/components/TestRedux.test.js
+++ src/components/TestRedux.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import { render, cleanup,  fireEvent } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import { initialState, reducer } from '../store/reducer'
 import TestRedux from './TestRedux'
 
@@ -15,23 +15,24 @@ const renderWithRedux = (
   }
 }
 
- afterEach(cleanup);
+afterEach(cleanup);
 
 it('checks initial state is equal to 0', () => {
-    const { getByTestId } = renderWithRedux(<TestRedux />)
-    expect(getByTestId('counter')).toHaveTextContent('0')
-  })
+  const { getByTestId } = renderWithRedux(<TestRedux />)
+  expect(getByTestId('counter')).toHaveTextContent('0')
+})
 
 it('increments the counter through redux', () => {
-  const { getByTestId } = renderWithRedux(<TestRedux />, 
-    {initialState: {count: 5}
-})
+  const { getByTestId } = renderWithRedux(<TestRedux />,
+    {
+      initialState: { count: 5 }
+    })
   fireEvent.click(getByTestId('button-up'))
   expect(getByTestId('counter')).toHaveTextContent('6')
 })
 
 it('decrements the counter through redux', () => {
-  const { getByTestId} = renderWithRedux(<TestRedux />, {
+  const { getByTestId } = renderWithRedux(<TestRedux />, {
     initialState: { count: 100 },
   })
   fireEvent.click(getByTestId('button-down'))
